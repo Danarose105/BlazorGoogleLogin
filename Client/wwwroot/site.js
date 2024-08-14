ca69bfda51794d07f62633fe801090dfb058a977
Danarose105 <103814272+Danarose105@users.noreply.github.com>
2024-08-14 14:25:41 +0300

    אונבורדינג עובד עד לפתיחת קטגוריה קיימת

--- Client/wwwroot/site.js
+++ Client/wwwroot/site.js
@@ -1,1 +1,1 @@
-﻿function getElementPosition(id) {    const element = document.getElementById(id);    return element.getBoundingClientRect();}
+﻿function getElementPosition(id) {    const element = document.getElementById(id);    return element.getBoundingClientRect();}
